c1a86fc3c0c8b776a8a3ca2262e1e241d0c8054f
Ganesh Nayanajith <ganeshnayanajith40@gmail.com>
2025-01-23 21:20:10 +0530

    port change

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(
 app.use("/payment", paymentRoutes);
 
 // Start the server
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
